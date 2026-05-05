4f78503d43ca4871f168aa40097d88df40b70a19
dominopancakes <antfoy@gmail.com>
2026-05-05 08:53:11 +0100

    add unit testing

---
+++ unit-testing-1/exercises/maths_example.py
@@ -0,0 +1,11 @@
+# a maths function
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# operation "add", "divide", "multiply", "subtract"
+def calculator(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
